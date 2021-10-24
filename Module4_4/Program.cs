d5f8cd9c3e061f99052cca5b573063a4f418e841
Danuta <Danuta@DANUTA-PC>
2021-10-24 22:06:51 +0300

    Add project files.

---
+++ Module4_4/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Module4_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Array1 = {12, 223, 3343, 45345, 23423, 23, 1112, 456, 33, 1};
+            int[] Array2 = new int[Array1.Length];
+            for (int i = 0; i < Array1.Length; i++)
+            {
+                Array2[i] = Array1[i];
+            }
+
+            Console.Write($"Array1: {string.Join(", ", Array1)} == Array2: {string.Join(", ", Array2)}");
+            Console.ReadLine();
+        }
+    }
+}
