502fd0b2d735dca1d5b6c1c8d550c216a2a25c8f
Danuta <Danuta@DANUTA-PC>
2021-10-25 17:35:33 +0300

    Another way how to assign values of array1 to array2, without using for loop

--- Module4_4/Program.cs
+++ Module4_4/Program.cs
@@ -6,14 +6,16 @@ namespace Module4_4
     {
         static void Main(string[] args)
         {
-            int[] Array1 = {12, 223, 3343, 45345, 23423, 23, 1112, 456, 33, 1};
-            int[] Array2 = new int[Array1.Length];
-            for (int i = 0; i < Array1.Length; i++)
-            {
-                Array2[i] = Array1[i];
-            }
+            int[] array1 = {12, 223, 3343, 45345, 23423, 23, 1112, 456, 33, 1};
+            int[] array2 = new int[array1.Length];
+            array1.CopyTo(array2, 0);
+            //for (int i = 0; i < array1.Length; i++)
+            //{
+            //    array2[i] = array1[i];
+            //}
 
-            Console.Write($"Array1: {string.Join(", ", Array1)} == Array2: {string.Join(", ", Array2)}");
+            Console.WriteLine($"array1: {string.Join(", ", array1)}");
+                Console.Write($"array2: {string.Join(", ", array2)}");
             Console.ReadLine();
         }
     }
