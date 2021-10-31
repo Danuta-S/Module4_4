bfa40306627b02058fd465c055840e6382734718
Danuta <Danuta@DANUTA-PC>
2021-10-31 20:17:17 +0200

    Rewritten with OOP approach.

---
+++ Module4_4/ArrayOperator.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module4_4
+{
+    public class ArrayOperator
+    {
+        public void CopyOfArray()
+        {
+            int[] array1 = { 12, 223, 3343, 45345, 23423, 23, 1112, 456, 33, 1 };
+            int[] array2 = array1;
+            Console.WriteLine($"array1: {string.Join(", ", array1)}");
+            Console.Write($"array2: {string.Join(", ", array2)}");
+            Console.ReadLine();
+        }
+
+        public int[] arr = new int[10];
+
+        public void InputValues()
+        {
+            for (var i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine($"Please input value with index {i}");
+
+                if (int.TryParse(Console.ReadLine(), out var input))
+                {
+                    arr[i] = input;
+                }
+                else
+                {
+                    Console.WriteLine("Incorrect input!");
+                }
+            }
+        }
+
+        public void CountDuplicates()
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] == arr[j])
+                    {
+                        Console.WriteLine($"Duplicate number is: {arr[i]}");
+                    }
+                }
+            }
+        }
+    }
+}
--- Module4_4/Program.cs
+++ Module4_4/Program.cs
@@ -2,55 +2,14 @@
 
 namespace Module4_4
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            int[] array1 = {12, 223, 3343, 45345, 23423, 23, 1112, 456, 33, 1};
-            int[] array2 = array1;
-
-            //int[] array2 = new int[array1.Length];
-
-            //array1.CopyTo(array2, 0);
-
-            //for (int i = 0; i < array1.Length; i++)
-            //{
-            //    array2[i] = array1[i];
-            //}
-
-            Console.WriteLine($"array1: {string.Join(", ", array1)}");
-                Console.Write($"array2: {string.Join(", ", array2)}");
-            Console.ReadLine();
-
-            #region Count all duplicates in the array
-            var arr = new int[10];
-
-            for (var i = 0; i < arr.Length; i++)
-            {
-                Console.WriteLine($"Please input value with index {i}");
-
-                if (int.TryParse(Console.ReadLine(), out var input))
-                {
-                    arr[i] = input;
-                }
-                else
-                {
-                    Console.WriteLine("Incorrect input!");
-                }
-            }
-
-            for (int i = 0; i < arr.Length; i++)
-            {
-                for (int j = i + 1; j < arr.Length; j++)
-                {
-                    if (arr[i] == arr[j])
-                    {
-                        Console.WriteLine($"Duplicate number is: {arr[i]}");
-                    }
-                }
-            }
-            Console.ReadLine();
-            #endregion
+            ArrayOperator arrayOperator = new ArrayOperator();
+            arrayOperator.CopyOfArray();
+            arrayOperator.InputValues();
+            arrayOperator.CountDuplicates();
         }
     }
 }
